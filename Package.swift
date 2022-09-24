7ade48c828c924148100690611e3b7c621fb2e58
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-24 11:39:56 -0500

    fixed manifest file

--- Package.swift
+++ Package.swift
@@ -6,23 +6,36 @@ import PackageDescription
 let package = Package(
     name: "RMVGeometry",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces,
+        // and make them visible to other packages.
         .library(
             name: "RMVGeometry",
-            targets: ["RMVGeometry"]),
+            targets: ["RMVCircle", "RMVGeometry"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // A target can define a module or a test suite.
+        // Targets can depend on other targets in this package,
+        // and on products in packages this package depends on.
+        .target(
+            name: "RMVCircle",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "RMVCircleTests",
+            dependencies: ["RMVGeometry"]
+        ),
         .target(
             name: "RMVGeometry",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "RMVGeometryTests",
-            dependencies: ["RMVGeometry"]),
+            dependencies: ["RMVGeometry"]
+        ),
     ]
 )

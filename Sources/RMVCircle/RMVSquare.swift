3082490394378c3bcabd4d700fc6dcf843f91865
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-25 10:23:07 -0500

    added RMVSquare

---
+++ Sources/RMVCircle/RMVSquare.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/// This struct calculates the characteristics of a square.
+public struct RMVSquare {
+    private let size: Double
+
+    /// Creates a `Square` with a given size.
+    /// - Parameter size: square width and height in any unit
+    public init(size: Double) {
+        self.size = size
+    }
+
+    /// Area of the sqaure in square units.
+    public var area: Double {
+        size * size
+    }
+}
